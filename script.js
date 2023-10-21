d16ee1c1c0a77b93bd234ef8a65ac4068ee704e8
Zev <140866110+Zevhys@users.noreply.github.com>
2023-10-21 11:46:40 +0800

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ var r = document.querySelector(":root");
 let workTittle = document.getElementById("work");
 let breakTittle = document.getElementById("break");
 
-let workTime = 1;
+let workTime = 25;
 let breakTime = 5;
 
 let seconds = "00";
